d8792ad704491ff06f9ea71c5b0896dfce954540
uran <9uranium2@gmail.com>
2017-04-24 17:38:24 +0900

    fix indent

--- components/swiper2.js
+++ components/swiper2.js
@@ -15,29 +15,29 @@ import { Button, ButtonGroup } from 'react-native-elements';
 
 class SexButton extends Component {
   constructor () {
-  super()
-  this.state = {
-    selectedIndex: 2
+    super()
+    this.state = {
+      selectedIndex: 2
+    }
+    this.updateIndex = this.updateIndex.bind(this)
   }
-  this.updateIndex = this.updateIndex.bind(this)
-}
-updateIndex (selectedIndex) {
-  this.setState({selectedIndex})
-}
+  updateIndex (selectedIndex) {
+    this.setState({selectedIndex})
+  }
 
-render () {
-  const buttons = ['女性', '男性']
-  const { selectedIndex } = this.state
-  return (
-    <ButtonGroup
-      onPress={this.updateIndex}
-      selectedIndex={selectedIndex}
-      buttons={buttons}
-      selectedBackgroundColor='steelblue'
-      selectedTextStyle={{color:'white'}}
-      containerStyle={{height: 40, width:250}} />
-  )
-}
+  render () {
+    const buttons = ['女性', '男性']
+    const { selectedIndex } = this.state
+    return (
+      <ButtonGroup
+        onPress={this.updateIndex}
+        selectedIndex={selectedIndex}
+        buttons={buttons}
+        selectedBackgroundColor='steelblue'
+        selectedTextStyle={{color:'white'}}
+        containerStyle={{height: 40, width:250}} />
+    )
+  }
 }
 
 class SexSegment extends Component {
